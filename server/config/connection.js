d88a31ccf88dc2cc981cb4ae3a452cefb4ae2940
monineathsar <monineathsar@gmail.com>
2024-01-27 14:28:57 -1000

    Added mongodb for local testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pawsitivePets_db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://pawsitivepals:dVbyUdwwD1lBUHSx@cluster0.ynyfnh2.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

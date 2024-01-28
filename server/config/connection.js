55e2d99e3e4eb5188a3934d2e505c0f8a22900e3
monineathsar <monineathsar@gmail.com>
2024-01-27 14:55:59 -1000

    Added default db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://pawsitivepals:dVbyUdwwD1lBUHSx@cluster0.ynyfnh2.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: 'pawsitive-pals'
 });
 
 module.exports = mongoose.connection;

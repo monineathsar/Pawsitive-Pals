7c0a888ba0a9f1e7ba53adb0dee0925543c6408c
monineathsar <monineathsar@gmail.com>
2024-03-02 11:19:16 -0800

    removed duplicated ApolloProvider code

--- client/src/App.js
+++ client/src/App.js
@@ -4,60 +4,35 @@ import SearchPets from './pages/SearchPets';
 import UserProfile from './pages/UserProfile';
 import AdminRequestPage from './pages/AdministratorPage';
 import AppNavbar from './components/Navbar';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import AnimalPage from './pages/AnimalProfile';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-        <>
-          <AppNavbar />
-          <Routes>
-            <Route
-              path='/'
-              element={<SearchPets />}
-            />
-            <Route
-              path='/saved'
-              element={<UserProfile />}
-            />
-            <Route
-              path='/admin'
-              element={<AdminRequestPage />}
-            />
-            <Route
-              path='/animal/:petId'
-              element={<AnimalPage />}
-            />
-            <Route
-              path='*'
-              element={<h1 className='display-2'>Wrong page!</h1>}
-            />
-          </Routes>
-        </>
-    </ApolloProvider >
+    <>
+      <AppNavbar />
+      <Routes>
+        <Route
+          path='/'
+          element={<SearchPets />}
+        />
+        <Route
+          path='/saved'
+          element={<UserProfile />}
+        />
+        <Route
+          path='/admin'
+          element={<AdminRequestPage />}
+        />
+        <Route
+          path='/animal/:petId'
+          element={<AnimalPage />}
+        />
+        <Route
+          path='*'
+          element={<h1 className='display-2'>Wrong page!</h1>}
+        />
+      </Routes>
+    </>
   );
 }
 
